7c9da45a738107c2ff3c2571e23b839f64fc16ac
Markow Kirill <kirillmarkor@gmail.com>
2023-02-12 14:35:34 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 z +
+            //int[] mas = new int[6];
+            //Random rnd = new Random();
+            //int i;
+            //for (i = 0; i <= 5; i++)
+            //{
+            //    mas[i] = rnd.Next(-10, 10);
+            //    Console.WriteLine($"mas[{i}]={mas[i]}");
+            //}
+            //Console.WriteLine();
+            //int index = -1;
+            //for (i = 0; i <= 5; i++)
+            //{
+            //    if (i >0  && mas[i] == mas[0])
+            //    {
+            //        index = i;
+            //        Console.WriteLine($"i={index}");
+            //    }
+            //}
+            //if (index == -1)
+            //{
+            //    Console.WriteLine("Чисел, равное первому элемента, нет");
+            //}
+            //Console.ReadLine();
+            // z 2 
+            //int[] mas = new int[20];
+            //Random rnd = new Random();
+            //int i;
+            //for (i = 0; i <= 19; i++)
+            //{
+            //    mas[i] = rnd.Next(-150, 202);
+            //    Console.WriteLine($"mas[{i}]={mas[i]}");
+            //}
+            //Console.WriteLine();
+
+            //string a = "", b = "", c = "";
+
+            //for (i = 0; i <= 19; i++)
+            //{
+            //    if (mas[i] > 0)
+            //    {
+            //        //Console.Write(mas[i]);
+            //        a = a + " " + mas[i].ToString();
+            //    }
+            //    if (mas[i]<=100)
+            //    {
+            //        b = b + " " + mas[i].ToString();
+            //    }
+            //    if (mas[i] % 2 !=0)
+            //    {
+            //        c = c + " " + mas[i].ToString();
+            //    }
+            //}
+
+            //Console.WriteLine("Все неотрицательные элементы: " + a);
+            //Console.WriteLine("Все элементы, не превышающие число 100: " + b);
+            //Console.WriteLine("Все четные элементы: " + c);
+
+            //Console.ReadKey();
+            // z 3 +
+            //int[] omas = new int[12] { 5, -3, 4, -8, -9, 1, 9, 6, -3, 4, 8, 2 };
+            //for(int i=0;i<=11;i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //int k=0;
+            //for (int i = 0; i <= 11; i++)
+            //{
+            //    if (omas[i] < 0) k++;
+            //}
+            //Console.WriteLine($"k={k}");
+            //Console.ReadKey();
+            // z 4
+            //int[] myArray = { 4, 7, 11, 2, 3 , 5 };
+            //float sum = 0;
+            //int k = 0; 
+            //for (int i = 0; i <= 3; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        sum=sum + myArray[i];
+            //        k++;
+            //    }
+            //}
+
+            //float sr_ar = sum / k;
+
+            //Console.WriteLine("Среднее арифметическое ={0}", sr_ar);
+            //Console.ReadKey();
+            // z 5 +
+            //int[] omas = new int[6];
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    Console.Write($"omas[{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //}
+            //for (int i = 0; i <= 5; i++)
+            //{
+            //    omas[i] += omas[0];
+            //}
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]= {omas[i]}");
+            //}
+            //Console.ReadKey();
+
+        }
+
+    }
+}
+            
+        
+    
+
